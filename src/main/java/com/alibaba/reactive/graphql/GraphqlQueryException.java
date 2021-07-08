14bcb24b0c2abfcda8b76e92da9bc513dc19ad1c
linux_china <libing.chen@gmail.com>
2021-07-08 13:12:56 -0700

    GraphqlQueryException

---
+++ src/main/java/com/alibaba/reactive/graphql/GraphqlQueryException.java
@@ -0,0 +1,21 @@
+package com.alibaba.reactive.graphql;
+
+import java.util.Map;
+
+/**
+ * GraphQL query exception
+ *
+ * @author linux_china
+ */
+public class GraphqlQueryException extends Exception {
+    private Map<String, Object> result;
+
+    public GraphqlQueryException(Map<String, Object> result) {
+        super(result.get("error").toString());
+        this.result = result;
+    }
+
+    public Map<String, Object> getResult() {
+        return result;
+    }
+}
