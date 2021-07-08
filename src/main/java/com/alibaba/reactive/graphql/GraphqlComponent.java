2814219703241ec2fdad4c0301bbf67fd6a32307
linux_china <libing.chen@gmail.com>
2021-07-08 13:13:35 -0700

    GraphqlComponent annotation

---
+++ src/main/java/com/alibaba/reactive/graphql/GraphqlComponent.java
@@ -0,0 +1,14 @@
+package com.alibaba.reactive.graphql;
+
+import java.lang.annotation.*;
+
+/**
+ * GraphQL Component
+ *
+ * @author linux_china
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface GraphqlComponent {
+}
